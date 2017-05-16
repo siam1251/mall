1cfb72731d8d0a07d57b85db3198b0dcce405f11
Martin Jeffers <mjeffers@kineticcafe.com>
2017-05-16 14:34:46 -0400

    [AI-301] Files for other targets

---
+++ app/src/seniondemo/java/floormapping/FloorToFloorMapPositionTranslator.java
@@ -0,0 +1,14 @@
+package floormapping;
+
+import java.util.HashMap;
+
+/**
+ * Created by martin on 2017-05-16.
+ */
+
+public class FloorToFloorMapPositionTranslator {
+
+    public static int getCorrectFloor(Integer floor){
+        return floor;
+    }
+}
