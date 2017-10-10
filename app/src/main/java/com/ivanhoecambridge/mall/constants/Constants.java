0cead8ba5c7e4392803a726430fc45e5d2a2ab8d
Doug Doe <ddoe@kineticcafe.com>
2017-10-10 11:45:11 -0400

    TODO complete. Cleanup.

--- app/src/main/java/com/ivanhoecambridge/mall/constants/Constants.java
+++ app/src/main/java/com/ivanhoecambridge/mall/constants/Constants.java
@@ -5,9 +5,7 @@ import com.ivanhoecambridge.mall.BuildConfig;
 import android.content.Context;
 
 public class Constants {
-    //todo: change production type in release
     public final static boolean IS_APP_IN_PRODUCTION = BuildConfig.IS_APP_IN_PRODUCTION;
-//	public final static boolean IS_APP_IN_PRODUCTION = false;
 
     public final static int  DURATION_MINIMUM_SPLASH_ANIMATION             = 1000;
     public final static int  DURATION_SPLASH_ANIMATION                     = 7000;
